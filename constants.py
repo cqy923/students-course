dfdfe589bd395c582ab471ddc2b6814444d3c054
cccc <74294337+cqy923@users.noreply.github.com>
2023-11-27 20:32:33 +0800

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,27 @@
+"""
+作者：刘昱江
+代码用途：
+日期：2022 年 09 月 28 日
+"""
+
+INVALID_KIND = "Invalid kind.kind should be student or teacher."
+ILLEGAL_KIND = "Illegal kind for you."
+INVALID_URL = "Invalid Url."
+
+INVALID_REQUEST_METHOD = "Invalid request method."
+
+COURSE_STATUS = {
+    1: "未开始选课",
+    2: "开始选课",
+    3: "结束选课",
+    4: "结课",
+    5: "打分完成",
+}
+
+COURSE_OPERATION = {
+    1: "开始选课",
+    2: "结束选课",
+    3: "结课",
+    4: "给分",
+    5: "查看详情"
+}
